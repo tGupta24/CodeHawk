f0a54b7275a6cba90469376fee37b4c542064c41
tarun <imtarungupta23@gmail.com>
2026-06-03 13:59:20 +0530

    nextJs setup

---
+++ my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button>hello world</Button>
+    </div>
+  );
+}
